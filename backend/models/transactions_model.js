8f919d5d6081507cf6867ee32104f161b4b3a4ca
Kata <kata.niva03@gmail.com>
2025-01-20 11:10:32 +0200

    transactions valmis

--- backend/models/transactions_model.js
+++ backend/models/transactions_model.js
@@ -6,6 +6,9 @@ const transactions={
     },
     getById:function(id, callback){
         return db.query('SELECT * FROM transactions WHERE idtransactions=?',[id],callback);
+    },
+    add:function(data, callback){
+        return db.query("insert into transactions(idaccount, datetime, transaction, amount) values(?,?,?,?)", [data.idaccount, data.datetime, data.transaction, data.amount], callback);
     }
 }
 
